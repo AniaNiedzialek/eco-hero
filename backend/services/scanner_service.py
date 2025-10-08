0187abd3dfdadb151a5591bfbb0a755613c948fa
Christina Kim <christina.kim@sjsu.edu>
2025-10-08 14:17:54 -0700

    add webscraping for barcode category

--- backend/services/scanner_service.py
+++ backend/services/scanner_service.py
@@ -1,6 +1,16 @@
 from pathlib import Path
+import time
 import cv2
 import zxingcpp
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import os
+from dotenv import load_dotenv
+import re
+
+# -------- Mock --------
+load_dotenv()
+MOCK = os.getenv('MOCK', 'False') == 'True'
 
 # -------- Paths --------
 DATA_DIR = Path(__file__).resolve().parents[1] / "data"
@@ -12,6 +22,7 @@ class BarcodeInfo:
         self.text = text
         self.format = format 
         self.position = position
+        self.category = "None"
 
 # -------- Helpers --------
 def scan_barcode(img_path=CA_BASELINE_PATH) -> list:
@@ -27,3 +38,36 @@ def scan_barcode(img_path=CA_BASELINE_PATH) -> list:
         barcodes.append(BarcodeInfo(barcode.text, barcode.format, barcode.position))
     
     return barcodes
+
+# Webscrape
+def set_category(barcodes=None):
+    if not barcodes:
+        _scrape_cat('5449000009067')
+    else:
+        for barcode in barcodes:
+            barcode.category = _scrape_cat(barcode.text)
+    return True
+
+def _scrape_cat(code: str) -> str:
+    url = f"https://www.barcodelookup.com/{code}"
+    if MOCK:
+        soup = BeautifulSoup(open('mock_data/barcode_page.html').read(), 'html.parser')
+    else:
+        html_content = _get_dynamic_page_source(url)
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+    category_div = soup.find_all('div', class_='product-text-label')
+    for div in category_div:
+        if div.get_text(strip=True).startswith('Category:'):
+            category = div.find('span', class_='product-text').get_text(strip=True)
+            print(category)
+            return category
+    return 'None'
+
+def _get_dynamic_page_source(url: str):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(5) # Adjust this delay as needed for content to load
+    page_source = driver.page_source
+    driver.quit()
+    return page_source
--- backend/tests/test_scanner_service.py
+++ backend/tests/test_scanner_service.py
@@ -1,6 +1,9 @@
 import pytest
-from services.scanner_service import scan_barcode
+from services.scanner_service import scan_barcode, set_category
 
 def test_scan_barcode():
     barcode = scan_barcode() 
     assert len(barcode) != 0
+
+def test_set_category():
+    assert set_category()
